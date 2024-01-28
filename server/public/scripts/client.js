eb64fe68d3a42b288ad6531be720d7251aebd252
Josh Rudesill <joshrudesill@gmail.com>
2024-01-28 14:11:02 -0600

    Base mode done

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,76 @@
-console.log('client.js is sourced!');
+console.log("client.js is sourced!");
+console.log("client.js is sourced!");
+let lastOperator = "";
+
+function onLoad() {
+  getHistory();
+}
+
+function getHistory() {
+  axios({
+    method: "GET",
+    url: "/calculations",
+  })
+    .then((res) => {
+      let lastRes = document.querySelector("#recentResult");
+      let history = document.querySelector("#resultHistory");
+      if (res.data.length > 0) {
+        lastRes.innerHTML = `<h2>${res.data.at(-1).result}</h2>`;
+        history.innerHTML = "";
+        res.data.forEach(
+          (row) =>
+            (history.innerHTML += `<p>${row.numOne} ${row.operator} ${row.numTwo} = ${row.result}</p>`)
+        );
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+function addCalculation(event) {
+  event.preventDefault();
+  let calculation = {
+    numOne: Number(document.getElementById("first").value),
+    numTwo: Number(document.getElementById("second").value),
+    operator: lastOperator,
+  };
+  axios({
+    method: "POST",
+    url: "/calculations",
+    data: calculation,
+  })
+    .then(function (_) {
+      clearForm();
+      getHistory();
+    })
+    .catch(function (error) {
+      console.log("error", error);
+    });
+}
+function setOperator(event, operator) {
+  lastOperator = operator;
+  document.querySelector("#equals").disabled = false;
+  clearOperator();
+
+  event.target.classList = "selected";
+}
+function clearOperator(withEquals = false) {
+  if (withEquals) {
+    lastOperator = "";
+    document.querySelector("#equals").disabled = true;
+  }
+
+  for (let child of document.querySelector("#calc-form").children) {
+    if (child.tagName === "BUTTON") {
+      child.classList.remove("selected");
+    }
+  }
+}
+function clearForm() {
+  document.querySelector("#equals").disabled = true;
+  document.getElementById("first").value = "";
+  document.getElementById("second").value = "";
+  clearOperator(true);
+}
+onLoad();
--- server/server.js
+++ server/server.js
@@ -1,28 +1,56 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 let PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(express.static('server/public'));
+app.use(express.static("server/public"));
 
 // Global variable that will contain all of the
 // calculation objects:
-let calculations = []
-
+let calculations = [];
 
 // Here's a wonderful place to make some routes:
 
 // GET /calculations
+app.get("/calculations", (req, res) => {
+  res.status(200);
+  res.send(calculations);
+});
+app.post("/calculations", (req, res) => {
+  let { numOne, numTwo, operator } = req.body;
+  // To pass the tests \/ - Data is already guarenteed to be sanitized from client but the tests spoof a request with strings.. so here we are
+  numOne = Number(numOne);
+  numTwo = Number(numTwo);
+  let calcResult;
+  switch (operator) {
+    case "+":
+      calcResult = numOne + numTwo;
+      break;
+    case "-":
+      calcResult = numOne - numTwo;
+      break;
+    case "/":
+      calcResult = numOne / numTwo;
+      break;
+    case "*":
+      calcResult = numOne * numTwo;
+      break;
+    default:
+      calcResult = 0;
+      break;
+  }
+  calculations.push({ ...req.body, result: calcResult });
+  res.sendStatus(201);
+});
 
 // POST /calculations
-
 
 // PLEASE DO NOT MODIFY ANY CODE BELOW THESE BEARS:
 // 🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸
 
 // Makes it so you don't have to kill the server
 // on 5000 in order to run the tests:
-if (process.env.NODE_ENV === 'test') {
+if (process.env.NODE_ENV === "test") {
   PORT = 5001;
 }
 
@@ -30,7 +58,7 @@ if (process.env.NODE_ENV === 'test') {
 // This is weird. We have to do it for testing reasons. There
 // is absolutely no need for you to reason about this.
 const server = app.listen(PORT, () => {
-  console.log('server running on: ', PORT);
+  console.log("server running on: ", PORT);
 });
 
 // server.setTimeout(500)
@@ -39,10 +67,10 @@ const server = app.listen(PORT, () => {
 // absolutely no need for you to reason about this.
 app.closeServer = () => {
   server.close();
-}
+};
 
 app.setCalculations = (calculationsToSet) => {
   calculations = calculationsToSet;
-}
+};
 
 module.exports = app;
